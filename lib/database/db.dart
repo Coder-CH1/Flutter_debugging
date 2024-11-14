78a20b46d092df5437f9bd65c67259482ca45b56
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-14 23:00:03 +0100

    added

--- lib/database/db.dart
+++ lib/database/db.dart
@@ -1,10 +1,1 @@
-import 'dart:io';
-import 'package:objectdb/objectdb.dart';
 
-final path = '${Directory.current.path}/my.db';
-// create database instance and open
-final db = ObjectDB(FileSystemStorage(path));
-
-class FileSystemStorage {
-  FileSystemStorage(String path);
-}

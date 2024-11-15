df7ba425ae723729061c035f66c4f9be567d5600
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-15 11:24:38 +0100

    added

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -5,13 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_debugging/reusable_widgets/colors.dart';
 import 'package:flutter_debugging/reusable_widgets/side_bar.dart';
 import 'package:flutter_emoji/flutter_emoji.dart';
-//import 'package:shared_preferences/shared_preferences.dart';
 import '../logger/logger.dart';
 
-//SharedPreferencesWithCache prefs = prefs;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //prefs = (await SharedPreferences.getInstance()) as SharedPreferencesWithCache;
   runApp(const MyApp());
 }
 
